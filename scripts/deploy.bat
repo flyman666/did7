6820c81563e0e906175770b640ab25e747b30db1
flyman666 <flyman666@gmail.com>
2023-05-18 10:29:08 +0800

    Create deploy.bat

---
+++ scripts/deploy.bat
@@ -0,0 +1,13 @@
+IF EXIST "public" (
+    xcopy "public" "..\.temp" /E /I
+    cd "..\.temp"
+    echo %cd%
+    git init
+    git add .
+    git commit -m "Posts update."
+    git remote add origin https://github.com/flyman666/did7.git
+    git push -f origin master
+    cd ..
+    rmdir /S /Q ".temp"
+    pushd D:\did7\
+)
